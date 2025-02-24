170e698653f1ddee40426d688dfcd5091e877878
TakuyaFujimura <fujimura.takuya@g.sp.m.is.nagoya-u.ac.jp>
2025-02-24 13:04:26 +0900

    added labels

--- jobs/labeling/run.sh
+++ jobs/labeling/run.sh
@@ -1,1 +1,7 @@
-bash ./base.sh dcase2023_dis_baseline
+bash ./base.sh dcase2021_msad
+bash ./base.sh dcase2022_msad
+bash ./base.sh dcase2023_msad
+bash ./base.sh dcase2024_msad
+
+bash ./base.sh dcase2021_ms
+bash ./base.sh dcase2022_ms
