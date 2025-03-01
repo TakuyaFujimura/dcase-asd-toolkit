b022e0ad7732c97cd2fbb281878d739c5b90068e
TakuyaFujimura <fujimura.takuya@g.sp.m.is.nagoya-u.ac.jp>
2025-03-01 17:50:01 +0900

    minor modification

--- jobs/asd/example/ae_baseline.sh
+++ jobs/asd/example/ae_baseline.sh
@@ -11,6 +11,3 @@ umap_exp="euclid"
 ckpt_ver_list="last"
 # ----------------------------------------------- #
 bash ../base/base.sh ${name} ${version} ${dcase} ${seed} ${extract_exp} ${score_exp} ${evaluate_exp} ${umap_exp} ${ckpt_ver_list}
-
-
-# TODO: Add evaluation of all machines
