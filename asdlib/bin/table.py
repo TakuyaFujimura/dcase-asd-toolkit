3e3bc93afb4a768b9cd630167b0857c30555a67f
TakuyaFujimura <fujimura.takuya@g.sp.m.is.nagoya-u.ac.jp>
2025-03-01 15:20:51 +0900

    added table

--- asdlib/bin/table.py
+++ asdlib/bin/table.py
@@ -3,61 +3,22 @@
 import logging
 from decimal import ROUND_HALF_UP, Decimal
 from pathlib import Path
-from typing import List
-
-import numpy as np
-import pandas as pd
-from scipy.stats import hmean
-
-logger = logging.getLogger(__name__)
-
-
-def myround(x: float) -> float:
-    return float(Decimal(str(x)).quantize(Decimal("0.01"), rounding=ROUND_HALF_UP))
-
-
-def get_table(metric: str, infer_dir: Path, machines: List[str]) -> pd.DataFrame:
-    df_list = []
-    col_list = []
-    backend_list = None
-    for m in machines:
-        df = pd.read_csv(infer_dir / m / "test_result.csv")
-        df_list.append(df[[metric]])
-        col_list.append(m)
-        if backend_list is None:
-            backend_list = df.backend.values
-        else:
-            assert np.all(backend_list == df.backend.values)
-    table_df = pd.concat(df_list, axis=1)
-    table_df.columns = col_list
-    table_df["hmean"] = table_df.apply(lambda x: hmean(x), axis=1)
-    table_df = table_df.applymap(lambda x: myround(x * 100))
-    table_df.index = backend_list
-    table_df = table_df.reset_index().rename(columns={"index": "backend"})
-    return table_df
-
-
-def table_main(metric_list: List[str], infer_dir: Path, machines: List[str]):
-    for metric in metric_list:
-        save_path = infer_dir / f"{metric}.csv"
-        logger.info(f"------------table of {metric}------------")
-        logger.info(f"save_path: {save_path}")
-        logger.info(f"machines: {machines}")
-        logger.info("-----------------------------------------------")
-        table_df = get_table(metric=metric, infer_dir=infer_dir, machines=machines)
-        table_df.to_csv(save_path, index=False)
-
-
-import logging
-from typing import Any, Dict, cast
+from typing import Any, Dict, List, cast
 
 import hydra
 import lightning.pytorch as pl
+import numpy as np
 import pandas as pd
 from hydra.core.hydra_config import HydraConfig
 from omegaconf import DictConfig, OmegaConf
+from pydantic import BaseModel
+from scipy.stats import hmean
 
+from asdlib.bin.utils.path import check_file_exists
 from asdlib.utils.config_class import MainTableConfig
+from asdlib.utils.dcase_utils import MACHINE_DICT
+
+logger = logging.getLogger(__name__)
 
 
 def hydra_to_pydantic(config: DictConfig) -> MainTableConfig:
@@ -66,7 +27,62 @@ def hydra_to_pydantic(config: DictConfig) -> MainTableConfig:
     return MainTableConfig(**config_dict)
 
 
-@hydra.main(version_base=None, config_path="../../config/umap", config_name="config")
+def myround(x: float) -> float:
+    return float(Decimal(str(x)).quantize(Decimal("0.01"), rounding=ROUND_HALF_UP))
+
+
+def get_table(metric: str, output_dir: Path, machine_list: List[str]) -> pd.DataFrame:
+    df_list = []
+    col_list = []
+
+    for i, m in enumerate(machine_list):
+        df = pd.read_csv(output_dir / m / "test_evaluate.csv")
+        df_list.append(df[[metric]])
+        col_list.append(m)
+        if i == 0:
+            backend_list = df.backend.values
+        elif np.all(backend_list == df.backend.values):
+            pass
+        else:
+            raise ValueError(
+                "Different backends are provided. "
+                + "This script assumes the same backends for all machines."
+            )
+    table_df = pd.concat(df_list, axis=1)
+    table_df.columns = col_list
+    table_df["hmean"] = table_df.apply(lambda x: hmean(x), axis=1)
+    table_df = table_df.applymap(lambda x: myround(x * 100))  # type: ignore
+    table_df.index = backend_list
+    table_df = table_df.reset_index().rename(columns={"index": "backend"})
+    return table_df
+
+
+class MetricMachineDict(BaseModel):
+    metric: str
+    machines: List[str]
+
+
+def get_metric_machine_dict(cfg: MainTableConfig) -> Dict[str, MetricMachineDict]:
+    metric_machine_dict = {}
+    yy = cfg.dcase[-2:]
+    if cfg.dcase in ["dcase2021", "dcase2022"]:
+        for split in ["dev", "eval"]:
+            metric_machine_dict[f"official{yy}-{split}"] = MetricMachineDict(
+                metric=f"official{yy}-{split}",
+                machines=MACHINE_DICT[f"{cfg.dcase}-{split}"],
+            )
+
+    elif cfg.dcase in ["dcase2023", "dcase2024"]:
+        for split in ["dev", "eval"]:
+            metric_machine_dict[f"official{yy}-{split}"] = MetricMachineDict(
+                metric=f"official{yy}",
+                machines=MACHINE_DICT[f"{cfg.dcase}-{split}"],
+            )
+
+    return metric_machine_dict
+
+
+@hydra.main(version_base=None, config_path="../../config/table", config_name="config")
 def main(hydra_cfg: DictConfig) -> None:
     cfg = hydra_to_pydantic(hydra_cfg)
     logger.info(f"Start scoring: {HydraConfig().get().run.dir}")
@@ -79,3 +95,20 @@ def main(hydra_cfg: DictConfig) -> None:
         / "output"
         / cfg.ckpt_ver
     )
+
+    check_file_exists(dir_path=output_dir, file_name="*.csv", overwrite=cfg.overwrite)
+
+    metric_machine_dict = get_metric_machine_dict(cfg=cfg)
+
+    for name in metric_machine_dict:
+        logger.info(f"Making table of {name}")
+        table_df = get_table(
+            metric=metric_machine_dict[name].metric,
+            output_dir=output_dir,
+            machine_list=metric_machine_dict[name].machines,
+        )
+        table_df.to_csv(output_dir / f"{name}.csv", index=False)
+
+
+if __name__ == "__main__":
+    main()
