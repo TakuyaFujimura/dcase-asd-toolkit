16a5a10f66e8f9812b5e61fa132e6bc84ed2e00b
TakuyaFujimura <fujimura.takuya@g.sp.m.is.nagoya-u.ac.jp>
2025-03-30 12:23:58 +0900

    git added jobs for me

---
+++ jobs/asd/call_dcase_seed_loop/ae_baseline/dcase2023.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#PJM -L rscgrp=cx-single
+#PJM -L gpu=4
+#PJM -L elapse=48:00:00
+#PJM -j
+# ---------------------------- #
+dcase_list=("dcase2023")
+# ---------------------------- #
+
+
+recipe_stem="ae_baseline"
+recipe="${recipe_stem}.sh"
+cd ../../recipe
+
+for dcase in "${dcase_list[@]}"; do
+    CUDA_VISIBLE_DEVICES=0 bash "${recipe}" "${dcase}" "0" > "../logs/${recipe_stem}/${dcase}_0.log" 2>&1 &
+    CUDA_VISIBLE_DEVICES=1 bash "${recipe}" "${dcase}" "1" > "../logs/${recipe_stem}/${dcase}_1.log" 2>&1 &
+    CUDA_VISIBLE_DEVICES=2 bash "${recipe}" "${dcase}" "2" > "../logs/${recipe_stem}/${dcase}_2.log" 2>&1 &
+    CUDA_VISIBLE_DEVICES=3 bash "${recipe}" "${dcase}" "3" > "../logs/${recipe_stem}/${dcase}_3.log" 2>&1 &
+    wait
+done
