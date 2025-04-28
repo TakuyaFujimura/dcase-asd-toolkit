95ed0b5950070a0eac9dff24670e472520199ed0
TakuyaFujimura <fujimura.takuya@g.sp.m.is.nagoya-u.ac.jp>
2025-04-28 18:33:20 +0900

    updated dis_beats

--- asdit/models/beats/lora.py
+++ asdit/models/beats/lora.py
@@ -1,52 +1,36 @@
 from pathlib import Path
 
-import torch
-from torch import nn
-
-from ..modules import AttnStatPool, add_lora
+from ..lora import BaseLoRA
 from .tools import resume
 
 
-class BEATsLoRA(nn.Module):
+class BEATsLoRA(BaseLoRA):
     def __init__(
         self,
-        sr: int,
-        embed_size: int = 128,
         ckpt_path: str = "pretrained_models/beats/BEATs_iter3.pt",
         lora_cfg: dict = {"r": 64, "target_modules": ["q_proj", "v_proj"]},
+        embed_size: int = 128,
         last_layer: str = "linear",
-        update_cfg: dict = {},
+        model_cfg: dict = {},
     ):
-        super().__init__()
+        super().__init__(
+            ckpt_path=ckpt_path,
+            lora_cfg=lora_cfg,
+            embed_size=embed_size,
+            last_layer=last_layer,
+            model_cfg=model_cfg,
+        )
+
+    def construct_model(self, ckpt_path: str, sr: int = 16000, update_cfg: dict = {}):
         if sr != 16000:
             raise ValueError("The sampling rate should be 16000")
+
         model = resume(ckpt_path=Path(ckpt_path), update_cfg=update_cfg)
-        self.model = add_lora(model, lora_cfg)
-        self.embed_size = embed_size
-        self.last_layer = last_layer
-        if self.last_layer == "linear":
-            self.network = nn.Linear(768, self.embed_size)
-        elif self.last_layer == "attn_stat_pool":
-            self.network = nn.Sequential(
-                AttnStatPool(embed_size=768), nn.Linear(768, self.embed_size)
-            )
+        return model, 768
 
-        else:
-            raise NotImplementedError(f"last_layer={self.last_layer} is not supported.")
-
-    def forward(self, x_time):
+    def extract_features(self, x):
         """
         Args
-            x_time: (B, L)
+            x: (B, L)
         """
-        z = self.model.extract_features(x_time)[0]  # (B, L, 768)
-
-        if self.last_layer == "linear":
-            z = self.network(z)  # (B, L, D)
-            z = torch.mean(z, dim=1)  # (B, D)
-        elif self.last_layer == "attn_stat_pool":
-            z = self.network(z)  # (B, D)
-        else:
-            raise NotImplementedError(f"last_layer={self.last_layer} is not supported.")
-
-        return z
+        return self.model.extract_features(x)[0]
---
+++ asdit/models/lora.py
@@ -0,0 +1,65 @@
+from abc import ABC, abstractmethod
+
+import torch
+from torch import nn
+
+from .modules import AttnStatPool, add_lora
+
+
+class BaseLoRA(nn.Module, ABC):
+    def __init__(
+        self,
+        ckpt_path: str,
+        lora_cfg: dict,
+        embed_size: int = 128,
+        last_layer: str = "linear",
+        model_cfg: dict = {},
+    ):
+        super().__init__()
+        model, feature_dim = self.construct_model(ckpt_path, **model_cfg)
+        self.model = add_lora(model, lora_cfg)
+        self.embed_size = embed_size
+        self.last_layer = last_layer
+        if self.last_layer == "linear":
+            self.network = nn.Linear(feature_dim, self.embed_size)
+        elif self.last_layer == "attn_stat_pool":
+            self.network = nn.Sequential(
+                AttnStatPool(embed_size=feature_dim),
+                nn.Linear(feature_dim, self.embed_size),
+            )
+
+        else:
+            raise NotImplementedError(f"last_layer={self.last_layer} is not supported.")
+
+    @abstractmethod
+    def construct_model(self, ckpt_path: str, **kwargs) -> tuple[nn.Module, int]:
+        """
+        Args:
+            ckpt_path (str): path to the checkpoint file
+
+        Returns:
+            SSL Model[nn.Module]: ssl model
+            feature dim [int]: feature dimension of the model
+        """
+        pass
+
+    @abstractmethod
+    def extract_features(self, x):
+        pass
+
+    def forward(self, x):
+        """
+        Args
+            x: (B, L)
+        """
+        z = self.extract_features(x)
+
+        if self.last_layer == "linear":
+            z = self.network(z)  # (B, L, D)
+            z = torch.mean(z, dim=1)  # (B, D)
+        elif self.last_layer == "attn_stat_pool":
+            z = self.network(z)  # (B, D)
+        else:
+            raise NotImplementedError(f"last_layer={self.last_layer} is not supported.")
+
+        return z
