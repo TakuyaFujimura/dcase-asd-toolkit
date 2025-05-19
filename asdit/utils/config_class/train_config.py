d9fca06e0c6e41945ca6dcb890dbede6eba7a337
TakuyaFujimura <fujimura.takuya@g.sp.m.is.nagoya-u.ac.jp>
2025-05-19 16:12:13 +0900

    minor modification

--- asdit/utils/config_class/train_config.py
+++ asdit/utils/config_class/train_config.py
@@ -17,7 +17,7 @@ class FrontendConfig(BaseModel):
     optim_cfg: Dict[str, Any]
     scheduler_cfg: Optional[Dict[str, Any]]
     grad_cfg: GradConfig
-    partially_saved_param_list: List[str] = Field(default_factory=list)
+    partially_saved_param_list: Optional[List[str]] = None
 
 
 class DMConfig(BaseModel):
