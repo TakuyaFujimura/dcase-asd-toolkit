943207e63ba68363bd7fa5cdcd6d1ec659e4451e
TakuyaFujimura <fujimura.takuya@g.sp.m.is.nagoya-u.ac.jp>
2025-06-27 17:10:15 +0900

    augmentation: added target_keys

--- asdit/augmentations/__init__.py
+++ asdit/augmentations/__init__.py
@@ -1,4 +1,3 @@
-from .cutmix import Cutmix
 from .featex import FeatEx
 from .mixup import Mixup
 from .specaug import SpecAug
--- asdit/augmentations/utils.py
+++ asdit/augmentations/utils.py
@@ -1,3 +1,5 @@
+import re
+
 import torch
 
 # def get_reverse_perm(batch_size: int, device: torch.device) -> torch.Tensor:
@@ -14,3 +16,10 @@ def get_dec(batch_size: int, prob: float, device: torch.device) -> torch.Tensor:
     dec = torch.rand(batch_size, device=device) < prob
     dec = dec.float()
     return dec
+
+
+def re_match_any(patterns: list[str], string: str) -> bool:
+    """
+    Check if any pattern matches the string.
+    """
+    return any(re.fullmatch(p, string) for p in patterns)
